c29c768fe1434a0f2c4d6bbbcbae43c300be63ca
shitritn <nissim.shitrit@microfocus.com>
2019-02-03 10:36:04 +0200

    add report

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -17,6 +17,22 @@ exports.config = {
 
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
+  },
+  
+  onPrepare: function() {
+  //var jasmineReporters = require('jasmine-node-reporter-fix');
+  //var capsPromise = browser.getCapabilities();
+  //capsPromise.then(function (caps) {
+    //jasmine.getEnv().addReporter(
+      //new jasmineReporters.JUnitXmlReporter(
+        //'protractor_output', true));
+	//});
+	var reporters = require('jasmine-reporters');
+	var junitReporter = new reporters.JUnitXmlReporter({
+		savePath: 'prot_output',
+		consolidateAll: false
+	});
+	jasmine.getEnv().addReporter(junitReporter);
   }
 
 };
